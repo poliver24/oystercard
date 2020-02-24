2d698a0f273c53f7bc9342f6e37d1fc2e9f097eb
plkujaw <jawor.jakub@gmail.com>
2020-02-24 15:54:55 +0000

    added and passed test to raise an error when #top_up value exceeds limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
 class Oystercard
 
   attr_reader :balance
+  LIMIT = 90
 
   def initialize
     @balance = 0
   end
 
   def top_up(value)
-    @balance = @balance + value
+    fail "Top up limit of #{LIMIT} exceeded" if value > LIMIT
+    @balance += value
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,8 +2,9 @@ require "oystercard"
 
 describe Oystercard do
 
-it "initializes with default balance of 0" do
-  expect(subject.balance).to eq 0
+  it "initializes with default balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 end
 
 describe "#top_up" do
@@ -14,6 +15,9 @@ describe "#top_up" do
     expect(subject.balance).to eq 10
     # expect { subject.top_up 1 }.to change{ subject.balance }.by 1
   end
-end
 
+  it "raises an error when #top_up value exceeds limit" do
+    card = Oystercard.new
+    expect{ subject.top_up(95) }.to raise_error("Top up limit of #{Oystercard::LIMIT} exceeded")
+  end
 end
