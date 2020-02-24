2d0c8f38a7379f8f20a6f27bfbb8c835b2e43687
Jara Santamaria <jarasmar@gmail.com>
2020-02-24 14:46:01 +0000

    First test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,5 @@
 class Oystercard
-
+  def balance
+    return 0
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,8 @@ require "oystercard"
 
 describe Oystercard do
 
+it "initializes with default balance of 0" do
+  expect(subject.balance).to eq 0
+end
+
 end
