2b5889ee07d9a5d4c5fd7b29faa3398d439105a6
plkujaw <jawor.jakub@gmail.com>
2020-02-25 13:16:33 +0000

    added test for top_up method to check if argument has been given

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,21 +3,26 @@ require "oystercard"
 describe OysterCard do
 
   it "initializes with default balance of 0" do
-    expect(subject.balance).to eq 0
+    expect(subject.balance).to eq(0)
   end
 
   describe "#top_up" do
 
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    # it "checks if #top_up value has been given" do
+    #   expect(subject).to respond_to(:top_up).with(1).argument
+    # end
+
     it "adds #top_up value to balance" do
-      card = OysterCard.new
-      expect { subject.top_up 10 }.to change{ subject.balance }.by 10
+      #card = OysterCard.new
+      expect { subject.top_up(10) }.to change{ subject.balance }.by(10)
 
       # subject.top_up(10)
       # expect(subject.balance).to eq 10
     end
 
     it "raises an error when #top_up value exceeds limit" do
-      card = OysterCard.new
+      #card = OysterCard.new
       expect{ subject.top_up(95) }.to raise_error("Top up limit of #{OysterCard::LIMIT} exceeded")
     end
   end
@@ -25,16 +30,16 @@ describe OysterCard do
   describe "#deduct" do
 
     it "#deducts value from balance" do
-      card = OysterCard.new
-      expect { subject.deduct 5 }.to change{ subject.balance }.by -5
+      # card = OysterCard.new
+      expect { subject.deduct(5) }.to change{ subject.balance }.by(-5)
     end
   end
 
   describe "#touch_in" do
 
     it "checks if card has been used to #touch_in" do
-      card = OysterCard.new
-      expect(subject.touch_in).to eq true
+      # card = OysterCard.new
+      expect(subject.touch_in).to eq(true)
     end
   end
 end
